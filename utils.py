86489b9f3d8355e12bff864812e9d72de2dc804a
ninzagamerz <106005123+ninzagamerz@users.noreply.github.com>
2024-09-17 21:03:08 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+from ftm import LOG_CHANNEL_ID, bot
+
+# Function to log user activity in the log channel
+async def log_user_activity(update: Update, context: CallbackContext):
+    user = update.effective_user
+    message = f"User {user.username} ({user.id}) accessed the bot."
+    await bot.send_message(chat_id=LOG_CHANNEL_ID, text=message)
