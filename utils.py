695fbd1cb62f68762ea9d320b57f1b809c07aaf6
ninzagamerz <106005123+ninzagamerz@users.noreply.github.com>
2024-09-18 08:39:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import CallbackContext
-from ftm import LOG_CHANNEL,
+from ftm import LOG_CHANNEL
 
 # Function to log user activity in the log channel
 async def log_user_activity(update: Update, context: CallbackContext):
